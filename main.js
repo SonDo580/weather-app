ae8c12d1c5545c57743d54f25fb15e86c4ea924a
Son Do <dominhson580@gmail.com>
2022-09-03 09:38:10 +0700

    Create function to extract the main data

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ const API_KEY = 'a23a2e333fd2e4f2e40a854e266d7ad2';
 
 async function getCurrentWeather(location) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`);
-    const weatherData = await response.json();
-    
-    console.log(weatherData);
+    const data = await response.json();
+
+    console.log(data);
+}
+
+function getNeededData(data) {
+    const weather = data.weather.main;
+    const temp = data.main.temp;
+    return { weather, temp };
 }
 
 getCurrentWeather('london');

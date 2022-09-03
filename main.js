cc4d2221a16f77dbe09e40fe9ddc9a78e04d6258
Son Do <dominhson580@gmail.com>
2022-09-03 09:22:46 +0700

    Create a function to get weather data

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const API_KEY = 'a23a2e333fd2e4f2e40a854e266d7ad2';
+
+async function getCurrentWeather(location) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`);
+    const weatherData = await response.json();
+    
+    console.log(weatherData);
+}
+
+getCurrentWeather('london');

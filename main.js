19ae8a2758706a757c9db8f433d819209508efe2
Son Do <dominhson580@gmail.com>
2022-09-03 10:45:08 +0700

    Return weatherDatato to use in other places

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const API_KEY = 'a23a2e333fd2e4f2e40a854e266d7ad2';
 async function getCurrentWeather(location) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`);
     const data = await response.json();
-    const weatherData = getNeededData(data);
-    console.log(weatherData);
+    return getNeededData(data);
 }
 
 function getNeededData(data) {
